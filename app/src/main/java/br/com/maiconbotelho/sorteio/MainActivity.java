3e97ce488aab12de37868b22b06a214a0b6b5e71
Maicon Botelho <maicon@maiconbotelho.com.br>
2023-11-09 18:13:02 -0300

    Primeiro Commit

---
+++ app/src/main/java/br/com/maiconbotelho/sorteio/MainActivity.java
@@ -0,0 +1,27 @@
+package br.com.maiconbotelho.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void sortearNumero(View view){
+
+        TextView texto = findViewById(R.id.textViewResultado);
+        int x = new Random().nextInt(11);
+        texto.setText("Número: " + x);
+
+    }
+}
